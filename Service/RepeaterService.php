46c7518b4bee3fc80639d96341ac099e3c0caf10
dave-yang <daworld.ny@gmail.com>
2024-03-24 02:29:48 +0500

    Added common methods in service

--- Service/RepeaterService.php
+++ Service/RepeaterService.php
@@ -23,4 +23,36 @@ final class RepeaterService
     {
         $this->repeaterMapper = $repeaterMapper;
     }
+
+    /**
+     * Returns last id
+     * 
+     * @return int
+     */
+    public function getLastId()
+    {
+        return $this->repeaterMapper->getMaxId();
+    }
+
+    /**
+     * Fetch all by collection id
+     * 
+     * @param int $collectionId
+     * @return array
+     */
+    public function fetchAll($collectionId)
+    {
+        return $this->repeaterMapper->fetchAll($collectionId);
+    }
+
+    /**
+     * Saves a repeater
+     * 
+     * @param array $input
+     * @return boolean
+     */
+    public function save(array $input)
+    {
+        return $this->repeaterMapper->batchInsert($input);
+    }
 }
