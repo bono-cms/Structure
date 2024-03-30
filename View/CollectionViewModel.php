050d8bfbe89d336966a3a9d065f40731255099e2
dave-yang <daworld.ny@gmail.com>
2024-03-30 23:12:40 +0500

    Added CollectionViewModel

---
+++ View/CollectionViewModel.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Structure\View;
+
+use Structure\Collection\LayoutCollection;
+use Structure\Collection\FieldTypeCollection;
+
+final class CollectionViewModel
+{
+    /**
+     * Get column classes by layout contant
+     * 
+     * @param int $layout
+     * @param array $fields
+     * @return array
+     */
+    public static function getColumns($layout, array $fields)
+    {
+        if ($layout == LayoutCollection::LAYOUT_LEFT_GRID_RIGHT_FORM) {
+            return [
+                'first' => 'col-lg-7',
+                'second' => 'col-lg-5'
+            ];
+        }
+
+        if ($layout == LayoutCollection::LAYOUT_TOP_GRID_BOTTOM_FORM) {
+            return [
+                'first' => 'col-lg-12',
+                'second' => 'col-lg-12'
+            ];
+        }
+
+        if ($layout == LayoutCollection::LAYOUT_AUTO) {
+            // Is there any WYSIWYG editors?
+            foreach ($fields as $field) {
+                if ($field['type'] == FieldTypeCollection::FIELD_WYSIWYG) {
+                    return [
+                        'first' => 'col-lg-12',
+                        'second' => 'col-lg-12'
+                    ];
+                }
+            }
+
+            return [
+                'first' => 'col-lg-7',
+                'second' => 'col-lg-5'
+            ];
+        }
+    }
+}
