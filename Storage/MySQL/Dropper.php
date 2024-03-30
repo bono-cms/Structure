d646cef83446876333bff7dc9baefc76e3942e5f
dave-yang <daworld.ny@gmail.com>
2024-03-30 14:42:51 +0500

    Added table dropper

---
+++ Storage/MySQL/Dropper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Structure\Storage\MySQL;
+
+use Cms\Storage\MySQL\AbstractStorageDropper;
+
+final class Dropper extends AbstractStorageDropper
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getTables()
+    {
+        return [
+            CollectionMapper::getTableName()
+            FieldMapper::getTableName()
+            RepeaterMapper::getTableName()
+            RepeaterValueMapper::getTableName()
+            RepeaterValueTranslationMapper::getTableName()
+        ];
+    }
+}
