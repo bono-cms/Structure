eba919b46d1ac0c57381be3586d224de1ea3e821
dave-yang <daworld.ny@gmail.com>
2024-04-11 14:23:17 +0500

    Added getPaginatedCollection() method

--- Service/SiteService.php
+++ Service/SiteService.php
@@ -28,22 +28,52 @@ final class SiteService
     public function __construct(RepeaterService $repeaterService, $langId)
     {
         $this->repeaterService = $repeaterService;
+        $this->setLangId($langId);
+    }
+
+    /**
+     * Define default language id
+     * 
+     * @param int $langId
+     * @return \Structure\Service\SiteService
+     */
+    public function setLangId($langId)
+    {
         $this->langId = $langId;
+        return $this;
+    }
+
+    /**
+     * Returns pagination instance
+     * 
+     * @return \Krystal\Paginate\Paginator
+     */
+    public function getPaginator()
+    {
+        return $this->repeaterService->getPaginator();
+    }
+
+    /**
+     * Returns paginated collection
+     * 
+     * @param int $id Collection ID
+     * @param int $page Current page
+     * @param int $itemsPerPage
+     * @return array
+     */
+    public function getPaginatedCollection($id, $page, $itemsPerPage = 10)
+    {
+        return $this->repeaterService->fetchPaginated($id, $this->langId, true, true, $page, $itemsPerPage);
     }
 
     /**
      * Returns collection data by its id
      * 
      * @param int $id Collection id
-     * @param int $langId Language id, in case of override
      * @return array
      */
-    public function getCollection($id, $langId = null)
+    public function getCollection($id)
     {
-        if (is_null($langId)) {
-            $langId = $this->langId;
-        }
-
-        return $this->repeaterService->fetchAll($id, $langId, true, true, true);
+        return $this->repeaterService->fetchAll($id, $this->langId, true, true, true);
     }
 }
